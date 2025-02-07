04f0482a950eac59ac7cf8ca1fc24cb995b39a31
RembertoJose <rembertowee@gmail.com>
2025-02-07 09:47:54 -0500

    Initial Commit

---
+++ DownTime-Shop/DownTime_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownTime_ShopApp.swift
+//  DownTime-Shop
+//
+//  Created by Remberto Nunez on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DownTime_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DownTime-ShopTests/DownTime_ShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  DownTime_ShopTests.swift
+//  DownTime-ShopTests
+//
+//  Created by Remberto Nunez on 2/7/25.
+//
+
+import Testing
+@testable import DownTime_Shop
+
+struct DownTime_ShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
